dce8d66456eb28e0f587cc37582cfc5d3dec3201
Anjan Kant <anjankant@hotmail.com>
2023-10-18 00:58:09 +0530

    Add project files.

---
+++ Cards/Card.cs
@@ -0,0 +1,134 @@
+﻿using Newtonsoft.Json;
+
+namespace Cards
+{
+    public class Card
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("manaCost")]
+        public string manaCost { get; set; }
+
+        [JsonProperty("cmc")]
+        public double? cmc { get; set; }
+
+        [JsonProperty("colors")]
+        public List<string> colors { get; set; }
+
+        [JsonProperty("colorIdentity")]
+        public List<string> colorIdentity { get; set; }
+
+        [JsonProperty("type")]
+        public string type { get; set; }
+
+        [JsonProperty("types")]
+        public List<string> types { get; set; }
+
+        [JsonProperty("subtypes")]
+        public List<string> subtypes { get; set; }
+
+        [JsonProperty("rarity")]
+        public string rarity { get; set; }
+
+        [JsonProperty("set")]
+        public string set { get; set; }
+
+        [JsonProperty("setName")]
+        public string setName { get; set; }
+
+        [JsonProperty("text")]
+        public string text { get; set; }
+
+        [JsonProperty("flavor")]
+        public string flavor { get; set; }
+
+        [JsonProperty("artist")]
+        public string artist { get; set; }
+
+        [JsonProperty("number")]
+        public string number { get; set; }
+
+        [JsonProperty("power")]
+        public string power { get; set; }
+
+        [JsonProperty("toughness")]
+        public string toughness { get; set; }
+
+        [JsonProperty("layout")]
+        public string layout { get; set; }
+
+        [JsonProperty("multiverseid")]
+        public string multiverseid { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string imageUrl { get; set; }
+
+        [JsonProperty("rulings")]
+        public List<Ruling> rulings { get; set; }
+
+        [JsonProperty("foreignNames")]
+        public List<ForeignName> foreignNames { get; set; }
+
+        [JsonProperty("printings")]
+        public List<string> printings { get; set; }
+
+        [JsonProperty("originalText")]
+        public string originalText { get; set; }
+
+        [JsonProperty("originalType")]
+        public string originalType { get; set; }
+
+        [JsonProperty("legalities")]
+        public List<Legality> legalities { get; set; }
+
+        [JsonProperty("id")]
+        public string id { get; set; }
+    }
+
+    public class ForeignName
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("text")]
+        public string text { get; set; }
+
+        [JsonProperty("type")]
+        public string type { get; set; }
+
+        [JsonProperty("flavor")]
+        public object flavor { get; set; }
+
+        [JsonProperty("language")]
+        public string language { get; set; }
+
+        [JsonProperty("multiverseid")]
+        public object multiverseid { get; set; }
+    }
+
+    public class Legality
+    {
+        [JsonProperty("format")]
+        public string format { get; set; }
+
+        [JsonProperty("legality")]
+        public string legality { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonProperty("cards")]
+        public List<Card> cards { get; set; }
+    }
+
+    public class Ruling
+    {
+        [JsonProperty("date")]
+        public string date { get; set; }
+
+        [JsonProperty("text")]
+        public string text { get; set; }
+    }
+
+}
---
+++ Cards/Controllers/Cards.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Cards.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Cards : ControllerBase
+    {
+        [HttpGet("GetCards")]
+        public IActionResult GetCards()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.magicthegathering.io/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("v1/cards?page=311").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok(response.Content.ReadAsStringAsync().Result);
+            }
+            else
+            {
+                return BadRequest("this is bad request.");
+            }
+        }
+
+
+        [HttpGet("GetCards/{name}")]
+        public IActionResult GetCardsByName(string name)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.magicthegathering.io/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("v1/cards?page=311").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = response.Content.ReadAsStringAsync().Result;
+                Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(jsonString);
+                var result = myDeserializedClass.cards.Where(x => x.name == name).ToList();
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest("this is bad request.");
+            }
+        }
+
+        [HttpGet("GetCardsByColor/{color}")]
+        public IActionResult GetCardsByColor(string color)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.magicthegathering.io/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("v1/cards?page=311").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = response.Content.ReadAsStringAsync().Result;
+                Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(jsonString);
+                var result = myDeserializedClass.cards.Where(x => x.colors.Contains(color)).ToList();
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest("this is bad request.");
+            }
+        }
+
+        [HttpGet("GetCardsByTypes/{type}")]
+        public IActionResult GetCardsByTypes(string type)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.magicthegathering.io/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("v1/cards?page=311").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = response.Content.ReadAsStringAsync().Result;
+                Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(jsonString);
+                var result = myDeserializedClass.cards.Where(x => x.types.Contains(type)).ToList();
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest("this is bad request.");
+            }
+        }
+    }
+}
